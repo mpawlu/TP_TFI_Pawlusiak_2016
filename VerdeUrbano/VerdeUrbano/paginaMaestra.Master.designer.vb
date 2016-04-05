58cfbaeecce4b36cdb64067044e209b909ede129
SantaT89 <santa.tassara@gmail.com>
2016-04-05 12:23:10 -0300

    05042016 1222S

--- VerdeUrbano/VerdeUrbano/paginaMaestra.Master.designer.vb
+++ VerdeUrbano/VerdeUrbano/paginaMaestra.Master.designer.vb
@@ -41,67 +41,13 @@ Partial Public Class paginaMaestra
     Protected WithEvents LogoMenu As Global.System.Web.UI.WebControls.Image
 
     '''<summary>
-    '''Control opcionesUsuario.
+    '''Control menuPrincipal.
     '''</summary>
     '''<remarks>
     '''Campo generado automáticamente.
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
-    Protected WithEvents opcionesUsuario As Global.System.Web.UI.HtmlControls.HtmlGenericControl
-
-    '''<summary>
-    '''Control img_Usuario.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents img_Usuario As Global.System.Web.UI.HtmlControls.HtmlImage
-
-    '''<summary>
-    '''Control lbl_NombredeUsuarioLogueado.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents lbl_NombredeUsuarioLogueado As Global.System.Web.UI.WebControls.Label
-
-    '''<summary>
-    '''Control lbl_apellidoyNombre.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents lbl_apellidoyNombre As Global.System.Web.UI.WebControls.Label
-
-    '''<summary>
-    '''Control img_mensajes.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents img_mensajes As Global.System.Web.UI.WebControls.ImageButton
-
-    '''<summary>
-    '''Control img_opciones.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents img_opciones As Global.System.Web.UI.WebControls.ImageButton
-
-    '''<summary>
-    '''Control img_logout.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents img_logout As Global.System.Web.UI.WebControls.ImageButton
+    Protected WithEvents menuPrincipal As Global.System.Web.UI.WebControls.Menu
 
     '''<summary>
     '''Control miContenidoPagina.
--- VerdeUrbano/VerdeUrbano/paginaMaestra.Master.vb
+++ VerdeUrbano/VerdeUrbano/paginaMaestra.Master.vb
@@ -2,6 +2,29 @@
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        cargarMenuEstatico()
+    End Sub
+
+
+    Private Sub cargarMenuEstatico()
+        Dim MiMenuInicio As New MenuItem
+        MiMenuInicio.NavigateUrl = "~/index.aspx"
+        MiMenuInicio.Text = "Inicio"
+        MiMenuInicio.Value = "Inicio"
+
+        Dim MiMenuInstitucional As New MenuItem
+        MiMenuInstitucional.NavigateUrl = "#"
+        MiMenuInstitucional.Text = "Institucional"
+        MiMenuInstitucional.Value = "Institucional"
+
+        Dim MiMenuServicios As New MenuItem
+        MiMenuServicios.NavigateUrl = "#"
+        MiMenuServicios.Text = "Servicios"
+        MiMenuServicios.Value = "Servicios"
+
+        menuPrincipal.Items.Add(MiMenuInicio)
+        menuPrincipal.Items.Add(MiMenuInstitucional)
+        menuPrincipal.Items.Add(MiMenuServicios)
 
     End Sub
 
