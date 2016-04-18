8429c0de1d8ad6dafc34a4aa96640cae4599efa4
SantaT89 <santa.tassara@gmail.com>
2016-04-18 15:08:37 -0300

    18042016 0308S

---
+++ VerdeUrbano/VerdeUrbano/agregarIdioma.aspx.designer.vb
@@ -0,0 +1,69 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class agregarIdioma
+
+    '''<summary>
+    '''Control cab_AgregarIdioma.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents cab_AgregarIdioma As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control lbl_Nombre.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_Nombre As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control txt_NombreIdioma.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txt_NombreIdioma As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control gv_Palabras.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents gv_Palabras As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control btn_agregar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btn_agregar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control btn_cancelar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btn_cancelar As Global.System.Web.UI.WebControls.Button
+End Class
