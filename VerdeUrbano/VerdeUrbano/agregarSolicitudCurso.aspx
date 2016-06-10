76504e63ca1595e7255024ce45127cc2d218ba45
SantaT89 <santa.tassara@gmail.com>
2016-06-10 10:21:39 -0300

    1021

---
+++ VerdeUrbano/VerdeUrbano/agregarSolicitudCurso.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/paginaMaestra.Master" CodeBehind="agregarSolicitudCurso.aspx.vb" Inherits="VerdeUrbano.agregarSolicitudCurso" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="contenidoPagina" runat="server">
+    <br />
+    <br />
+    <div class="row">
+        <div class="well well-lg col-md-12 msj-ok" runat="server" visible="false" id="correcto">
+            <asp:Label ID="lbl_AccionCorrecta" runat="server" CssClass="labelOK"></asp:Label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="well well-lg col-md-12 msj-error" runat="server" visible="false" id="error">
+            <asp:Label ID="lbl_TituloError" runat="server" CssClass="labelError"></asp:Label>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ VerdeUrbano/VerdeUrbano/agregarSolicitudCurso.aspx.designer.vb
@@ -0,0 +1,51 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class agregarSolicitudCurso
+    
+    '''<summary>
+    '''Control correcto.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents correcto As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+    
+    '''<summary>
+    '''Control lbl_AccionCorrecta.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_AccionCorrecta As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control error.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents [error] As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+    
+    '''<summary>
+    '''Control lbl_TituloError.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_TituloError As Global.System.Web.UI.WebControls.Label
+End Class
