cc96f941be408940e2a2b469b760a8b97d55e748
SantaT89 <santa.tassara@gmail.com>
2016-06-10 14:21:30 -0300

    10062016 0221

--- VerdeUrbano/VerdeUrbano/agregarSolicitudCurso.aspx.designer.vb
+++ VerdeUrbano/VerdeUrbano/agregarSolicitudCurso.aspx.designer.vb
@@ -48,4 +48,139 @@ Partial Public Class agregarSolicitudCurso
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents lbl_TituloError As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control lbl_AgregarUsuario.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_AgregarUsuario As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control lbl_titulo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_titulo As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control txtTitulo.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtTitulo As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control RequiredFieldValidator1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents RequiredFieldValidator1 As Global.System.Web.UI.WebControls.RequiredFieldValidator
+    
+    '''<summary>
+    '''Control lbl_detalle.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_detalle As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control txtDetalle.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtDetalle As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control RequiredFieldValidator2.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents RequiredFieldValidator2 As Global.System.Web.UI.WebControls.RequiredFieldValidator
+    
+    '''<summary>
+    '''Control lbl_categoria.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_categoria As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control ddlCategoria.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents ddlCategoria As Global.System.Web.UI.WebControls.DropDownList
+    
+    '''<summary>
+    '''Control lbl_fechaCreacion.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lbl_fechaCreacion As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control txtFechaCreacion.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents txtFechaCreacion As Global.System.Web.UI.WebControls.TextBox
+    
+    '''<summary>
+    '''Control Label1.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Label1 As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''Control gv_Profesores.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents gv_Profesores As Global.System.Web.UI.WebControls.GridView
+    
+    '''<summary>
+    '''Control btnCalculador.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnCalculador As Global.System.Web.UI.WebControls.Button
+    
+    '''<summary>
+    '''Control btnSiguiente.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents btnSiguiente As Global.System.Web.UI.WebControls.Button
 End Class
--- VerdeUrbano/VerdeUrbano/agregarSolicitudCurso.aspx.vb
+++ VerdeUrbano/VerdeUrbano/agregarSolicitudCurso.aspx.vb
@@ -2,7 +2,22 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            obtenerUsuarios()
 
+        End If
     End Sub
 
+    Private Sub obtenerUsuarios()
+        Try
+            Dim _bllUsuario As New BLL.clsUsuario
+            Dim _listaUsuarios As New List(Of Servicios.Usuario)
+            _listaUsuarios = _bllUsuario.ListarUsuarios
+            Me.gv_Profesores.DataSource = _listaUsuarios
+            Me.gv_Profesores.DataBind()
+        Catch ex As Exception
+            Me.error.Visible = True
+            Me.lbl_TituloError.Text = ex.Message
+        End Try
+    End Sub
 End Class
