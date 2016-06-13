46931ead67c1cf5ce5d8d664ed26377444a8d856
SAntaT89 <DTH880amatista>
2016-06-12 21:53:56 -0300

    2153 Santa

--- VerdeUrbano/VerdeUrbano/evaluacion.aspx.vb
+++ VerdeUrbano/VerdeUrbano/evaluacion.aspx.vb
@@ -7,34 +7,26 @@
 
     Private Sub generarPreguntas(ByVal paramListadoPreguntas As List(Of EE.Pregunta))
         Try
-            'Dim _contador As Integer = 1
-            'For Each MiPregunta As EE.Pregunta In paramListadoPreguntas
-            '    Dim label As Label = Me.panelPreguntas.FindControl("lbl_pregunta" & _contador)
-            '    Dim labelID As Label = Me.panelPreguntas.FindControl("id_" & _contador)
-            '    label.Text = MiPregunta.Pregunta
-            '    labelID.Text = MiPregunta.ID
-            '    _contador += 1
-            '    rb_pregunta1.DataSource = MiPregunta.Opciones
-
-            'Next
-
-            '    rb_pregunta1.DataSource =
-            '    rb_pregunta1.DataBind()
-            '    rb_pregunta2.DataSource = System.Enum.GetValues(GetType(Entidades.PreguntaOpinion.etipoRespuesta))
-            '    rb_pregunta2.DataBind()
-            '    rb_pregunta3.DataSource = System.Enum.GetValues(GetType(Entidades.PreguntaOpinion.etipoRespuesta))
-            '    rb_pregunta3.DataBind()
-            '    rb_pregunta4.DataSource = System.Enum.GetValues(GetType(Entidades.PreguntaOpinion.etipoRespuesta))
-            '    rb_pregunta4.DataBind()
-            '    rb_pregunta5.DataSource = System.Enum.GetValues(GetType(Entidades.PreguntaOpinion.etipoRespuesta))
-            '    rb_pregunta5.DataBind()
-            'Catch ex As BLL.excepcionGenerica
-            '    Me.Error.Visible = True
-            '    Me.lbl_TituloError.Text = ex.Mensaje
+            Dim _contador As Integer = 1
+            For Each MiPregunta As EE.Pregunta In paramListadoPreguntas
+                Dim label As Label = Me.panelPreguntas.FindControl("lbl_pregunta" & _contador)
+                Dim labelID As Label = Me.panelPreguntas.FindControl("id_" & _contador)
+                label.Text = MiPregunta.Pregunta
+                labelID.Text = MiPregunta.ID
+                _contador += 1
+                rb_pregunta1.DataSource = MiPregunta.Opciones
+                Dim radio As RadioButtonList = Me.panelPreguntas.FindControl("rb_pregunta" & _contador)
+                radio.DataSource = MiPregunta.Opciones
+                radio.DataBind()
+            Next
         Catch ex As Exception
             '    Me.Error.Visible = True
             '    Me.lbl_TituloError.Text = ex.Message
         End Try
+
+    End Sub
+
+    Protected Sub btnFinalizar_Click(sender As Object, e As EventArgs) Handles btnFinalizar.Click
 
     End Sub
 End Class
