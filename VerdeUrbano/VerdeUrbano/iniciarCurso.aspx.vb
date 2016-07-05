ea94cbe3739234bfb80361c2e68d993eb7b5fc9e
Programadora <Programadora@PROGRAMADORA-PC>
2016-07-05 09:53:27 -0300

    953

--- VerdeUrbano/VerdeUrbano/iniciarCurso.aspx.vb
+++ VerdeUrbano/VerdeUrbano/iniciarCurso.aspx.vb
@@ -2,9 +2,9 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        If validaciones.validarPagina(Me) = False Then
-            Response.Redirect("error.aspx")
-        End If
+        'If validaciones.validarPagina(Me) = False Then
+        '    Response.Redirect("error.aspx")
+        '  End If
         If Not IsPostBack Then
             Response.Redirect("encuestaSatisfaccion.aspx")
             Dim oCursoAs As New EE.CursoAsignado
